4b5e72c909e97831411fa3de289b2f8cec383851
hanjize <hanjize@rd.tuan800.com>
2014-02-10 14:15:09 +0800

    something change
    something change

---
+++ HHobjectLiterals Tests/HHobjectLiterals_Tests.m
@@ -0,0 +1,34 @@
+//
+//  HHobjectLiterals_Tests.m
+//  HHobjectLiterals Tests
+//
+//  Created by LeAustinHan on 14-2-7.
+//  Copyright (c) 2014年 hanhan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HHobjectLiterals_Tests : XCTestCase
+
+@end
+
+@implementation HHobjectLiterals_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
